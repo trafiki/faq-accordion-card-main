7569d3a488d28ad731fc13531b0f4f6a35e1891d
Babatunde Akerele <babsake@gmail.com>
2021-07-20 16:53:44 +0100

    complete desktop design

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let faqItem = document.querySelectorAll('li.faq-item');
-for(let i=0; i<faqItem.length; i++){
-  faqItem[i].addEventListener('click', ()=> {
+
+for (let i = 0; i < faqItem.length; i++) {
+  faqItem[i].addEventListener('click', () => {
     faqItem[i].classList.toggle('active');
+
   })
 }
+
+// let cube = document.querySelector('img.cube');
+//
+// faqItem.addEventListener('mouseover', () => {
+//   console.log('seen');
+//   alert('hovered');
+//   cube.style.left = "-130px";
+// })
